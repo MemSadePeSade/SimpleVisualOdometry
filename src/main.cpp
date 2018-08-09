7e0ad3a3001a5578828d6d340f85202c3ef06bca
Виктор Пономарев <vponomarev@tsk.ntrlab.ru>
2018-08-09 11:36:08 +0700

    Format text

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,25 @@
 #include "vo_features.h"
 
 struct CameraParam {
-	CameraParam() : pp(607.1928, 185.2157),intrisic_mat(1, 0, 0, 0, 1, 0, 0, 0, 1) {}
-	cv::Mat     dist_coeff ;
+	CameraParam() : pp(607.1928, 185.2157), intrisic_mat(1, 0, 0, 0, 1, 0, 0, 0, 1) {}
+	cv::Mat     dist_coeff;
 	cv::Matx33f intrisic_mat;
-	float focal_length = 718.8560;  
+	float focal_length = 718.8560;
 	cv::Point2f pp;
 };
 
 void MakeIntrisicMatFromVector(CameraParam& camera_param,
-	                           const std::vector<float>& parametrs) {
+	const std::vector<float>& parametrs) {
 	camera_param.intrisic_mat(0, 0) = parametrs[0]; //fx
 	camera_param.intrisic_mat(0, 2) = parametrs[2]; //cx
 	camera_param.intrisic_mat(1, 1) = parametrs[1]; //fy
 	camera_param.intrisic_mat(1, 2) = parametrs[3]; //cy
-    camera_param.focal_length = std::sqrt(std::pow(2, parametrs[0]) + std::pow(2, parametrs[1]));
+	camera_param.focal_length = std::sqrt(std::pow(2, parametrs[0]) + std::pow(2, parametrs[1]));
 	camera_param.pp.x = parametrs[2];
 	camera_param.pp.y = parametrs[3];
 }
 
-int LoadCameraParam(const std::string& filename, CameraParam& camera_param){
+int LoadCameraParam(const std::string& filename, CameraParam& camera_param) {
 	FileStorage fs;
 	fs.open(filename, FileStorage::READ);
 	if (!fs.isOpened())
@@ -33,7 +33,7 @@ int LoadCameraParam(const std::string& filename, CameraParam& camera_param){
 		cerr << "Failed to open " << filename << endl;
 		return 1;
 	}
-	
+
 	FileNode n = fs["cam0"];
 	FileNodeIterator it = n.begin(), it_end = n.end(); // Go through the node
 	for (; it != it_end; ++it)
@@ -61,7 +61,7 @@ int LoadCameraParam(const std::string& filename, CameraParam& camera_param){
 		}
 	}
 	fs.release();
- 	return 0;
+	return 0;
 }
 
 
@@ -72,12 +72,12 @@ int main(int argc, char** argv) {
 	double fontScale = 1;
 	int thickness = 1;
 	cv::Point textOrg(10, 50);
-	
+
 	std::string filename = "C://data//z.yaml";
 	CameraParam camera_param;
-	
+
 	//LoadCameraParam(filename,camera_param);
-	
+
 	//cv::namedWindow("Road facing camera1", WINDOW_AUTOSIZE);// Create a window for display.
 	cv::namedWindow("Road facing camera2", WINDOW_AUTOSIZE);// Create a window for display.
 	cv::namedWindow("Trajectory", WINDOW_AUTOSIZE);// Create a window for display.
@@ -91,33 +91,33 @@ int main(int argc, char** argv) {
 	// cycle through the directory
 	Ptr<FeatureDetector> featureDetector;
 	if (!createDetectorDescriptorMatcher("ORB", featureDetector))
-	   return -1;
+		return -1;
 
 	cv::Mat traj = Mat::zeros(600, 600, CV_8UC3);
-	cv::Matx31d t_f(0,0,0);
+	cv::Matx31d t_f(0, 0, 0);
 	cv::Matx33d R_f(1, 0, 0, 0, 1, 0, 0, 0, 1);
 	cv::Mat E_prev;
-	for (prev_itr,curr_itr; curr_itr != end_itr; ++curr_itr,++prev_itr){
+	for (prev_itr, curr_itr; curr_itr != end_itr; ++curr_itr, ++prev_itr) {
 		Mat img_prev = imread(prev_itr->path().string());
 		Mat img_curr = imread(curr_itr->path().string());
 		cvtColor(img_prev, img_prev, COLOR_BGR2GRAY);
 		cvtColor(img_curr, img_curr, COLOR_BGR2GRAY);
-		
+
 		// feature detection, tracking
 		vector<Point2f> points1, points2;//vectors to store the coordinates of the feature points
 		featureDetection(featureDetector, img_prev, points1, "NeFast");//detect features in img1
 		vector<uchar> status;
 		featureTracking(img_prev, img_curr, points1, points2, status);
-		
+
 		cv::Mat img2_keypoints;
 		std::vector<cv::KeyPoint> draw_points2;
 		draw_points2.resize(points2.size());
-		std::for_each(draw_points2.begin(), draw_points2.end(), 
+		std::for_each(draw_points2.begin(), draw_points2.end(),
 			[&points2, counter = 0](auto& it)  mutable
-		    {
-			     it.pt = points2[counter];
-				 counter++;
-		    });
+		{
+			it.pt = points2[counter];
+			counter++;
+		});
 		drawKeypoints(img_curr, draw_points2, img2_keypoints, Scalar::all(-1), DrawMatchesFlags::DEFAULT);
 		/*
 		cv::Mat img1_keypoints;
@@ -131,25 +131,25 @@ int main(int argc, char** argv) {
 		});
 		drawKeypoints(img1, draw_points1, img1_keypoints, Scalar::all(-1), DrawMatchesFlags::DEFAULT);
 		*/
-		
+
 		cv::Matx31d t;
 		cv::Matx33d R;
 		Mat E, mask;
-		E = cv::findEssentialMat(points1, points2, camera_param.focal_length, 
-			                     camera_param.pp, RANSAC, 0.999, 1.0, mask);
+		E = cv::findEssentialMat(points1, points2, camera_param.focal_length,
+			camera_param.pp, RANSAC, 0.999, 1.0, mask);
 		// undistortion
-		if(!E_prev.empty())
-		  cv::undistortPoints(points1, points1, E_prev, camera_param.dist_coeff);
+		if (!E_prev.empty())
+			cv::undistortPoints(points1, points1, E_prev, camera_param.dist_coeff);
 		else
 			E_prev = cv::Mat::eye(3, 3, CV_64F);
 		cv::undistortPoints(points2, points2, E, camera_param.dist_coeff);
 		E_prev *= E;
 		// estimate R,T
 		cv::recoverPose(E, points1, points2, R, t, camera_param.focal_length,
-			            camera_param.pp, mask);
-		
+			camera_param.pp, mask);
+
 		double scale = 1.0;
-		t_f = scale*t + scale * (R * t_f);
+		t_f = scale * t + scale * (R * t_f);
 
 		int x = int(t_f(0)) + 300;
 		int y = int(t_f(2)) + 100;
@@ -165,9 +165,9 @@ int main(int argc, char** argv) {
 		//cv::imshow("Road facing camera1", img1_keypoints);
 		cv::imshow("Road facing camera2", img2_keypoints);
 		cv::imshow("Trajectory", traj);
-	
-		if (cv::waitKey(0)  == 27)
-		   break;
+
+		if (cv::waitKey(0) == 27)
+			break;
 	}
 
 	cv::destroyAllWindows();
--- src/vo_features.h
+++ src/vo_features.h
@@ -19,30 +19,30 @@
 using namespace cv;
 using namespace std;
 
-void featureTracking(Mat img_1, Mat img_2, vector<Point2f>& points1, vector<Point2f>& points2, vector<uchar>& status)	{ 
-//this function automatically gets rid of points for which tracking fails
-  vector<float> err;					
-  Size winSize=Size(21,21);																								
-  TermCriteria termcrit=TermCriteria(TermCriteria::COUNT+TermCriteria::EPS, 30, 0.01);
+void featureTracking(Mat img_1, Mat img_2, vector<Point2f>& points1, vector<Point2f>& points2, vector<uchar>& status) {
+	//this function automatically gets rid of points for which tracking fails
+	vector<float> err;
+	Size winSize = Size(21, 21);
+	TermCriteria termcrit = TermCriteria(TermCriteria::COUNT + TermCriteria::EPS, 30, 0.01);
 
-  calcOpticalFlowPyrLK(img_1, img_2, points1, points2, status, err, winSize, 3, termcrit, 0, 0.001);
-  //getting rid of points for which the KLT tracking failed or those who have gone outside the frame
-  int indexCorrection = 0;
-  for( int i=0; i<status.size(); i++){  
-	  Point2f pt = points2.at(i- indexCorrection);
-     	if ((status.at(i) == 0)||(pt.x<0)||(pt.y<0)){
-     		  if((pt.x<0)||(pt.y<0)){
-     		  	status.at(i) = 0;
-     		  }
-     		  points1.erase (points1.begin() + (i - indexCorrection));
-     		  points2.erase (points2.begin() + (i - indexCorrection));
-     		  indexCorrection++;
-     	}
-     }
+	calcOpticalFlowPyrLK(img_1, img_2, points1, points2, status, err, winSize, 3, termcrit, 0, 0.001);
+	//getting rid of points for which the KLT tracking failed or those who have gone outside the frame
+	int indexCorrection = 0;
+	for (int i = 0; i < status.size(); i++) {
+		Point2f pt = points2.at(i - indexCorrection);
+		if ((status.at(i) == 0) || (pt.x < 0) || (pt.y < 0)) {
+			if ((pt.x < 0) || (pt.y < 0)) {
+				status.at(i) = 0;
+			}
+			points1.erase(points1.begin() + (i - indexCorrection));
+			points2.erase(points2.begin() + (i - indexCorrection));
+			indexCorrection++;
+		}
+	}
 }
 
 static bool createDetectorDescriptorMatcher(const string& detector_type,
-	                                        Ptr<FeatureDetector>& detector){
+	Ptr<FeatureDetector>& detector) {
 	cout << "<Creating feature detector" << endl;
 	if (detector_type == "SIFT") {
 		detector = cv::xfeatures2d::SIFT::create();
@@ -56,13 +56,13 @@ static bool createDetectorDescriptorMatcher(const string& detector_type,
 	bool isCreated = !detector.empty();
 	if (!isCreated)
 		cout << "Can not create feature detector of given types." << endl << ">" << endl;
-    return isCreated;
+	return isCreated;
 }
 
 void featureDetection(const Ptr<FeatureDetector>& detector,
-	                  const Mat& img, 
-	                  vector<Point2f>& points, 
-	                  std::string mode){
+	const Mat& img,
+	vector<Point2f>& points,
+	std::string mode) {
 	vector<KeyPoint> keypoints;
 	if (mode == "FAST") {
 		//uses FAST as of now, modify parameters as necessary
@@ -70,7 +70,7 @@ void featureDetection(const Ptr<FeatureDetector>& detector,
 		bool nonmaxSuppression = true;
 		FAST(img, keypoints, fast_threshold, nonmaxSuppression);
 	}
-	else { 
+	else {
 		detector->detect(img, keypoints);
 	}
 	KeyPoint::convert(keypoints, points, vector<int>());
